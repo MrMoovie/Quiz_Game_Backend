3ceea46230513058fae09994090e9b44189d2c81
Sir Benjy <starvinbergvelvel@gmail.com>
2026-03-10 16:46:20 +0200

    first commit

---
+++ src/main/java/com/ashcollege/entities/ClientEntity.java
@@ -0,0 +1,14 @@
+package com.ashcollege.entities;
+
+public class ClientEntity extends BasicUser {
+    private String address;
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
